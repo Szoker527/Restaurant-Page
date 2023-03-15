3ef0e7d62bbb3d6722309af72f5aead1dca47b1f
Szoker527 <szoker527@gmail.com>
2023-03-15 16:14:35 +0100

    Finished Contact page

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
 # Restaurant-Page
-<a href="https://www.freepik.com/free-photo/solid-concrete-wall-textured-backdrop_17839221.htm#query=chalkboard%20background&position=21&from_view=keyword&track=ais">Image by rawpixel.com</a> on Freepik
 
+<h2>Images links</h2>
+<a href="https://www.freepik.com/free-photo/solid-concrete-wall-textured-backdrop_17839221.htm#query=chalkboard%20background&position=21&from_view=keyword&track=ais">Image by rawpixel.com</a> on Freepik
 <a href="https://www.pexels.com/photo/chicken-wings-on-a-wooden-board-13999253/">Chicken wings</a> photo by Leonardo Luz.
+Photo by <a href=" https://www.pexels.com/photo/deliciously-looking-pizza-with-parma-ham-10875298/">Valeria Boltneva</a> from Pexels
+Photo by <a href=" https://www.pexels.com/photo/pizza-pepperoni-with-salami-and-fresh-basil-leaves-cut-in-triangles-10875209/">Valeria Boltneva</a> from Pexels
+Photo by <a href="https://www.pexels.com/photo/fried-potatoes-1583884/">Dzenina</a> from Pexels
+Photo by <a href="https://www.pexels.com/photo/cooked-pizza-1146760/">Kristina Paukshtite</a> from Pexels
+Photo by <a href="https://www.pexels.com/photo/a-close-up-shot-of-a-delicious-margherita-pizza-14391207/">Leonardo Luz</a> from Pexels
+Photo by <a href="https://www.pexels.com/photo/chicken-wings-on-a-wooden-board-13999253/">Leonardo Luz</a> from Pexels
+<a href="https://www.freepik.com/free-photo/solid-concrete-wall-textured-backdrop_17839221.htm">Solid concrete wall textured backdrop</a>
+<p>Images in contact are taken JoJo's Bizarre Adventure anime</p>
+<h2>Fonts links</h2>
+<a href="https://www.fontspace.com/dk-midnight-chalker-font-f24683">DK Midnight Chalker Font</a>
+<a href="https://www.fontspace.com/chalktastic-font-f32933 ">Chalktastic Font</a>
--- src/contact.js
+++ src/contact.js
@@ -1,20 +1,59 @@
 export default contactPage; 
 
 function removeAllChildNodes(parent) {
-    while (parent.firstChild) {
-      parent.removeChild(parent.firstChild);
-    }
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
   }
+}
 
 function contactPage() {
-    const container = document.getElementById("display")
-    if (container.hasChildNodes()) {
-        console.log("myDiv has child nodes.");
-        removeAllChildNodes(container)
-      }
-      
-    const placeholder = document.createElement("div")
-    placeholder.textContent = "Contact!"
-    placeholder.classList.add("contact")
-    container.appendChild(placeholder)
+const container = document.getElementById("display")
+if (container.hasChildNodes()) {
+    console.log("myDiv has child nodes.");
+    removeAllChildNodes(container)
+  }
+
+  const containerHome = document.createElement("div")
+  containerHome.classList.add("home-flex")
+  container.appendChild(containerHome)
+  console.log(containerHome)
+
+  const h2 = document.createElement("h2")
+  h2.textContent = "Contact"
+  h2.classList.add("menu")
+  containerHome.appendChild(h2)
+
+  createContactBox(containerHome, "Joseph Joestar", "Chef", "+696 696 696", "thistruemail@gmail.com", "jojo-image")
+  createContactBox(containerHome, "Suzi Q", "Kelner", "+121 121 121", "callmeq@gmail.com", "suzi-image")
+  createContactBox(containerHome, "Robert E. O. Speedwagon", "Cleaner", "+666 666 6", "speedwagon@gmail.com", "speed-image")
+}
+
+function createContactBox(container, names, position, phone, email, images) {
+const contactBox = document.createElement("div")
+const img = document.createElement("div")
+const descriptionSide = document.createElement("div")
+container.appendChild(contactBox)
+
+contactBox.appendChild(descriptionSide)
+contactBox.appendChild(img)
+contactBox.classList.add("contact-box")
+
+const name = document.createElement("h3")
+const positionName = document.createElement("p")
+const phoneNumber = document.createElement("p")
+const emailtext = document.createElement("p")
+
+img.classList.add(`${images}`);
+
+descriptionSide.classList.add("contact-context");
+
+name.textContent = `${names}`
+positionName.textContent = `${position}`
+phoneNumber.textContent = `${phone}`
+emailtext.textContent = `${email}`
+
+descriptionSide.appendChild(name)
+descriptionSide.appendChild(positionName)
+descriptionSide.appendChild(phoneNumber)
+descriptionSide.appendChild(emailtext)
 }
